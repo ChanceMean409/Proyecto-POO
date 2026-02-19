8443e216f29c270847b2da691f3916ecedbb2a92
rlopr <rlopr@Rodrigo>
2026-02-19 16:08:54 -0600

    productos

--- src/Tienda.java
+++ src/Tienda.java
@@ -2,9 +2,47 @@
 import java.util.Scanner;
 public class Tienda {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         cliente c=new cliente();
+        productos.inventario();
         c.registro();
-        c.verificar();
+        boolean puedeEntrar = c.verificar();
+        
+        if (puedeEntrar){
+            System.out.println("\n========================================");
+            System.out.println("    BIENBENIDOS A LA TIENDA DE ABARROTES");
+            System.out.println("========================================");
+            carrito miCarrito = new carrito();
+            int opcion;
+            int opcionSalir = productos.catalogo.size();
+            
+            do {
+                System.out.println("\n--- Pasillo de Productos ---");
+                for (int i = 0; i < productos.catalogo.size(); i++) {
+                    productos p =productos.catalogo.get(i);
+                    System.out.println(i+". "+p.getNombre()+" -  $"+p.getPrecio());
+                }
+                System.out.println(opcionSalir+". ir a la caja a pagar (Salir)");
+                System.out.println(opcionSalir+"\nElija el numero de producto que desea: ");
+                opcion = sc.nextInt();
+                if (opcion>=0&&opcion<opcionSalir) {
+                    productos productoElegido = productos.catalogo.get(opcion);
+                    if (productoElegido.verificarDisponibilidad(1)) {
+                        miCarrito.agregarProductos(productoElegido);
+                        productoElegido.actualizarStock(1);
+                    }else{
+                        System.out.println("Lo sentimos, producto agotado.");
+                    }
+                }else if (opcion!= opcionSalir){
+                    System.out.println("Opcion no valida. intente de nuevo.");
+                }
+            } while (opcion!=opcionSalir);
+            miCarrito.mostrarDetalle();
+            System.out.println("\n¡Gracias por su compra!");
+            
+        } else {
+            System.out.println("\nCerrando el sistema por seguridad");
+        }
     }
     
 }
--- src/productos.java
+++ src/productos.java
@@ -26,16 +26,16 @@ public class productos {
       public static void inventario() {
         if (!catalogo.isEmpty()) return; // evita duplicados
 
-        catalogo.add(new productos("pan", 38,59, "harinas"));
-        catalogo.add(new productos("atun",35, 20, "enlatados"));
-        catalogo.add(new productos("chiplotles", 15, 48, "enlatados"));
-        catalogo.add(new productos("vasos de unicel", 20, 30, "desechables"));
-        catalogo.add(new productos("coca cola", 54, 20, "bebidas"));
-        catalogo.add(new productos("leche", 25, 58, "lacteos"));
-        catalogo.add(new productos("queso", 15, 10, "lacteos"));
-        catalogo.add(new productos("sopa", 10, 48, "harinas"));
-        catalogo.add(new productos("mayonesa", 35, 7, "enlatados"));
-        catalogo.add(new productos("corn flakes", 57, 17, "harinas"));
+        catalogo.add(new productos("Pan", 38,59, "harinas"));
+        catalogo.add(new productos("Atun",35, 20, "enlatados"));
+        catalogo.add(new productos("Chiplotles", 15, 48, "enlatados"));
+        catalogo.add(new productos("Vasos de unicel", 20, 30, "desechables"));
+        catalogo.add(new productos("Coca cola", 54, 20, "bebidas"));
+        catalogo.add(new productos("Leche", 25, 58, "lacteos"));
+        catalogo.add(new productos("Queso", 15, 10, "lacteos"));
+        catalogo.add(new productos("Sopa", 10, 48, "harinas"));
+        catalogo.add(new productos("Mayonesa", 35, 7, "enlatados"));
+        catalogo.add(new productos("Corn flakes", 57, 17, "harinas"));
     }
     // Verificar disponibilidad
     public boolean verificarDisponibilidad(int inventario) {
