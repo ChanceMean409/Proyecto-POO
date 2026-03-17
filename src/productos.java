d2e5165eeb8854cc8cc3fa4abd786993a7140169
gordi <gordi@DESKTOP-FPFLTTH>
2026-03-16 20:05:29 -0600

    esa huevada

--- src/productos.java
+++ src/productos.java
@@ -15,7 +15,7 @@ public class productos {
         stock = 0;
         categoria = "";
     }
-    
+
     //CONSTRUCTOR con argumentos
     public productos(String nombre, float precio, int stock, String categoria) {
         this.nombre = nombre;
@@ -23,26 +23,18 @@ public class productos {
         this.stock = stock;
         this.categoria = categoria;
     }
-    
-    //METODO DE INVENTARIO PREDETERMINADO
-      public static void inventario() {
-        if (!catalogo.isEmpty()) return; // evita duplicados
-        
-        catalogo.add(new productos("Pan de elote (2 piezas)",15,59, "Harinas"));
-        catalogo.add(new productos("Pan Choncha de vainilla (1 unidad)", 15,39, "Harinas"));
-        catalogo.add(new productos("Atun en agua",25, 20, "Enlatados"));
-        catalogo.add(new productos("Atun en aceite",35, 25, "Enlatados"));
-        catalogo.add(new productos("Chiplotles (bolsa pequena)", 15, 48, "Enlatados"));
-        catalogo.add(new productos("Vasos desechables (10 vasos)", 20, 30, "Desechables"));
-        catalogo.add(new productos("Coca cola 3L", 54, 20, "Bebidas"));
-        catalogo.add(new productos("Leche", 31, 58, "Lacteos"));
-        catalogo.add(new productos("Queso Oaxaca", 55, 10, "Lacteos"));
-        catalogo.add(new productos("Queso Fresco", 35, 10, "Lacteos"));
-        catalogo.add(new productos("Sopa", 7, 48, "Harinas"));
-        catalogo.add(new productos("Mayonesa", 35, 10, "Enlatados"));
-        catalogo.add(new productos("Corn flakes", 55, 17, "Harinas"));
+        // nueva forma de inventario
+    public static ArrayList<Perecederos> listaPerecederos = new ArrayList<>();
+    public static ArrayList<NoPedecederos> listaNoPerecederos = new ArrayList<>();
+    // creacion de los inventario al general
+     public static void generarCatalogo(){
+
+        if(!catalogo.isEmpty()) return;
+
+        catalogo.addAll(listaPerecederos);
+        catalogo.addAll(listaNoPerecederos);
     }
-      
+   
     // METODO para verificar disponibilidad
     public boolean verificarDisponibilidad(int inventario) {
         if (inventario > 0 && stock >= inventario) {
